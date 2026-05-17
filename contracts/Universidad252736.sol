0016e055b2e04afde5c2962715daaf89e197f15a
Yerson0811 <yerson0811@gmail.com>
2026-05-16 20:09:39 -0500

    Parte 3

--- contracts/Universidad252736.sol
+++ contracts/Universidad252736.sol
@@ -9,26 +9,39 @@ contract Biblioteca252736 {
         uint id;
         string titulo;
         uint paginas;
+        bool estado;
     }
 
     Libro[] public libros;
 
     address public dirContrato;
 
-    constructor() {
+    constructor() ejecutadoPor{
         dirContrato = address(this);
+    }
+
+    modifier ejecutadoPor() {
         console.log(
             "Ejecutado por: 252736 - Yerson Enrique Diaz Paredes"
         );
+        _;
     }
 
-    function agregarElemento(uint _id, string memory _titulo, uint _paginas) public {
+    function agregarElemento(uint _id, string memory _titulo, uint _paginas) public ejecutadoPor{
+        //Check de ID no repetido
+        for(uint i = 0; i < libros.length; i++) {
+            require(libros[i].id != _id,"ID repetido");
+        }
+
+        //Check de titulo vacio
+        require(bytes(_titulo).length > 0,"Titulo vacio");
+
         libros.push(
-            Libro(_id, _titulo, _paginas)
+            Libro(_id, _titulo, _paginas, true)
         );
     }
 
-    function contarElementos() public view returns(uint) {
+    function contarElementos() public view ejecutadoPor returns(uint) {
         return libros.length;
     }
 
