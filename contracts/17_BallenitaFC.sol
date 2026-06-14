78874a99ddfebf709e67b5e83301098a7dbbc0d0
Yerson0811 <yerson0811@gmail.com>
2026-06-13 20:13:50 -0500

    8th commit

--- contracts/17_BallenitaFC.sol
+++ contracts/17_BallenitaFC.sol
@@ -3,26 +3,36 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BallenitaFCToken is ERC20{
+contract BallenitaFCToken is ERC20,  Ownable{
 
-    address public owner = msg.sender;
+    uint256 public tokenPrice = 0.001 ether;
 
-    constructor () ERC20("Ballenita Futbol Club Token", "BFC") {
-        owner = msg.sender;
-        _mint(owner, 1000);
+    constructor () ERC20("Ballenita Futbol Club Token", "BFC") Ownable(msg.sender){
+        _mint(owner(), 1000 * 10**18);
     }
 
-    modifier isOwner(){
-        require(msg.sender == owner, "No eres el propietario");
-        _;
-    }
-
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount * 10 ** 18);
     }
 
     function isPartner(address account) public view returns (bool){
         return balanceOf(account) > 0;
+    }
+
+    function balanceOfBFT(address account) public view returns (uint256){
+        return balanceOf(account) / 10**18;
+    }
+
+    function buyTokens() public  payable {
+        require(msg.value>0, "Debes enviar un valor minimo de ETH");
+        uint256 amountBFT = msg.value / tokenPrice;
+        require(amountBFT>0, "El ETH enviado es insuficiente");
+        mint(msg.sender, amountBFT);
+    }
+
+    function withdraw() public onlyOwner {
+        payable (owner()).transfer(address(this).balance);
     }
 }
