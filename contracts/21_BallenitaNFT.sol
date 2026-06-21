27d69a0461912032800c4a7e0cb8bcd2590514d3
Yerson0811 <yerson0811@gmail.com>
2026-06-20 21:35:32 -0500

    commit #10

--- contracts/21_BallenitaNFT.sol
+++ contracts/21_BallenitaNFT.sol
@@ -21,19 +21,45 @@ contract BallenitaNFT is ERC721,  Ownable{
         string name;
         uint256 level;
         uint256 rarity;
+        uint256 price;
     }
 
     mapping(uint256 => Poster) private posters;
 
-    function mint(address to, string memory _name, uint256 _level, uint256 _rarity) public onlyOwner {
+    function mint(string memory _name, uint256 _level, uint256 _rarity, uint256 _price) public onlyOwner {
         uint256 posterId = _counterId;
-        posters[posterId] = Poster(posterId, _name, _level, _rarity);
-        _safeMint(to, _counterId);
+        posters[posterId] = Poster(posterId, _name, _level, _rarity, _price);
+        _mint(address(this), _counterId);
         _counterId++;
     }
 
-    function showPoster(uint256 tokenId) public view returns (uint256, string memory, uint256, uint256){
+    function showPoster(uint256 tokenId) public view returns (uint256, string memory, uint256, uint256, uint256) {
         Poster memory poster = posters[tokenId];
-        return (poster.id, poster.name, poster.level, poster.rarity);
+        return (poster.id, poster.name, poster.level, poster.rarity, poster.price);
+    }
+
+    function verPostersProp(address owner) public view returns (Poster[] memory) {
+        uint256 posterAmount = balanceOf(owner);
+        Poster[] memory result = new Poster[](posterAmount);
+        uint256 counter = 0;
+        for(uint256 tokenId = 1; tokenId < _counterId; tokenId++ ) {
+            
+            if(ownerOf(tokenId) == owner) {
+                result[counter] = posters[tokenId];
+                counter++;
+            }
+        }
+        return result;
+    }
+
+    function buyPoster (uint256 tokenId) public payable {
+        require(ownerOf(tokenId) == address(this), "El poster no esta disponible");
+        Poster memory poster = posters[tokenId];
+        require(msg.value >= poster.price, "ETH Insuficiente ");
+        _transfer(address(this), msg.sender, tokenId);
+    }
+
+    function withdraw() public onlyOwner {
+        payable (owner()).transfer(address(this).balance);
     }
 }
---
+++ contracts/22_ReuseContract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Calculator{
+    function calculateFinalGrade(uint256 teo, uint256 pra, uint256 lab) public pure returns(uint){
+        //uint256 FG = (teo * 30 + pra * 30 + lab * 40) / 100;
+        uint256 FG = (teo * 10 + pra * 10 + lab * 80) / 100;
+        return FG;
+    }
+}
+
+contract RecordGrades is Ownable{
+
+    address public  addressCalculator;
+
+    mapping (address => uint256) public studentGrades;
+
+    constructor (address _addressCalculator) Ownable(msg.sender){
+        addressCalculator = _addressCalculator;
+    }
+
+    function updateCalculator (address _addressCalculator) public onlyOwner{
+        addressCalculator = _addressCalculator;
+    }
+
+    function recordGrades(address _dirStudent, uint256 _teo, uint256 _pra, uint256 _lab) public onlyOwner {
+        Calculator calculator = Calculator(addressCalculator);
+        uint256 FG = calculator.calculateFinalGrade(_teo, _pra, _lab);
+        studentGrades[_dirStudent] = FG;
+    }
+
+    function viewGrade() public view returns(uint256){
+        return studentGrades[msg.sender];
+    }
+}
